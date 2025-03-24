54fddd42b67c127c6d0592cf80a820efc55a2b48
Aditya Mehta <aditya.mehta@iitgn.ac.in>
2025-03-24 19:54:08 +0530

    Lab 6

--- Assignment2/Lab6/terminal_commands.sh
+++ Assignment2/Lab6/terminal_commands.sh
@@ -11,8 +11,5 @@ for ((i=1; i<=10; i++)); do pytest | tee "sequential_run_$i.log"; done
 
 for ((i=1; i<=3; i++)); do pytest | tee "sequential_run_final_$i.log"; done
 
-pytest -n <num_workers> --dist <distribution_mode> --parallel-threads <num_threads>
-
-pytest -n auto --dist load --parallel-threads 1
-
-pytest -n 1 --dist no --parallel-threads auto
+# Rest of the commands are given in algorithms/runner.py
+python algorithms/runner.py
